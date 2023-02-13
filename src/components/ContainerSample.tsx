dd3537a38aaa1e47c5fdc44024a7e092499915e4
iwairikuto <wjrikuto@gmial.com>
2023-02-13 16:46:46 +0900

    3.3

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -1,10 +1,13 @@
 //Containerは赤背景のボックスの中にタイトルと子要素を表示します
 import React from "react";
 
-const Container= (props: {
+//containerの型
+type ContainerProps = {
     title:string;
     children:React.ReactNode;
-}) => {
+}
+
+const Container= (props: ContainerProps) => {
     const {title, children} = props;
 
     return (
@@ -15,7 +18,7 @@ const Container= (props: {
     )
 }
 
-const Parent = () => {
+const Parent = (): JSX.Element => {
     return (
         <Container title={"Hello"}>
             <p>ここの部分が背景色で囲まれます</p>
